52580bf1f427eda6d6c8a23cf38c72fabbce0132
Ameya Gadkari <ameyagadkari@hotmail.com>
2018-02-03 11:26:17 -0700

    Swipe Control for panning done

--- cgol/Assets/Scripts/CameraController.cs
+++ cgol/Assets/Scripts/CameraController.cs
@@ -14,7 +14,9 @@ namespace Assets.Scripts
         private Vector3 _originalPostion;
         private MinMax<float> _orthographicSize;
         private Bounds _bounds;
+        private Vector3 _initialMousePosition;
 
+        private const float PanSpeed = 2.5f;
         private const float ZoomSpeed =
 #if UNITY_EDITOR || UNITY_STANDALONE
         100.0f;
@@ -57,19 +59,32 @@ namespace Assets.Scripts
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                transform.position = _originalPostion;
-                transform.rotation = Quaternion.identity;
-                _camera.orthographicSize = _orthographicSize.Max;
-                _bounds.Update(_camera);
-            }
+            ResetCamera();
+            DetermineZoomDelta();
+        }
 
+        private void LateUpdate()
+        {
+            ZoomCamera();
+            PanCamera();
+        }
+
+        private void ResetCamera()
+        {
+            if (!Input.GetKeyDown(KeyCode.R)) return;
+            transform.position = _originalPostion;
+            transform.rotation = Quaternion.identity;
+            _camera.orthographicSize = _orthographicSize.Max;
+            _bounds.Update(_camera);
+            transform.position = _bounds.ClampCamera(transform.position);
+        }
+
+        private void DetermineZoomDelta()
+        {
 #if UNITY_EDITOR || UNITY_STANDALONE
             _zoomInputDelta = -Input.GetAxis("Mouse ScrollWheel");
 #elif UNITY_ANDROID || UNITY_IOS
-        if (Input.touchCount == 2)
-        {
+            if (Input.touchCount != 2) return;
             var touchZero = Input.GetTouch(0);
             var touchOne = Input.GetTouch(1);
 
@@ -80,26 +95,38 @@ namespace Assets.Scripts
             var currentTouchDeltaMagnitude = (touchZero.position - touchOne.position).magnitude;
 
             _zoomInputDelta = previousTouchDeltaMagnitude - currentTouchDeltaMagnitude;
-        }
 #endif
         }
 
-        private void LateUpdate()
+        private void ZoomCamera()
         {
-            if (Mathf.Abs(_zoomInputDelta) > 0.0f)
-            {
-                var zoomDeltaTimeIndependent = _zoomInputDelta * Time.smoothDeltaTime * ZoomSpeed;
-                _camera.orthographicSize += zoomDeltaTimeIndependent;
-                _camera.orthographicSize =
-                    Mathf.Clamp(_camera.orthographicSize, _orthographicSize.Min, _orthographicSize.Max);
-                _bounds.Update(_camera);
-                _zoomInputDelta = 0.0f;
-            }
+            if (!(Mathf.Abs(_zoomInputDelta) > 0.0f)) return;
+            _camera.orthographicSize += _zoomInputDelta * Time.smoothDeltaTime * ZoomSpeed;
+            _camera.orthographicSize =
+                Mathf.Clamp(_camera.orthographicSize, _orthographicSize.Min, _orthographicSize.Max);
+            _bounds.Update(_camera);
+            _zoomInputDelta = 0.0f;
+            transform.position = _bounds.ClampCamera(transform.position);
+        }
 
-            var position = transform.position;
-            position.x = Mathf.Clamp(position.x, _bounds.Left, _bounds.Right);
-            position.y = Mathf.Clamp(position.y, _bounds.Bottom, _bounds.Top);
-            transform.position = position;
+        private void PanCamera()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _initialMousePosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                var direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) -
+                                Camera.main.ScreenToWorldPoint(_initialMousePosition);
+                direction.z = 0.0f;
+
+                var position = transform.position + direction;
+
+                var newPosition = Vector3.Lerp(transform.position,
+                    new Vector3(position.x, position.y, transform.position.z), Time.smoothDeltaTime * PanSpeed);
+                transform.position = _bounds.ClampCamera(newPosition);
+            }
         }
     }
 
@@ -112,18 +139,18 @@ namespace Assets.Scripts
         private Vector3 _bottomLeft;
         private Vector3 _topRight;
 
-        public float Left { get; private set; }
-        public float Right { get; private set; }
-        public float Top { get; private set; }
-        public float Bottom { get; private set; }
+        private float _left;
+        private float _right;
+        private float _top;
+        private float _bottom;
 
         public Bounds(Camera camera, float max)
         {
             _vExtent = camera.orthographicSize;
             _hExtent = camera.aspect * _vExtent;
 
-            _bottomLeft = camera.ViewportToWorldPoint(Vector3.zero);
-            _topRight = camera.ViewportToWorldPoint(Vector3.one);
+            _bottomLeft = camera.ViewportToWorldPoint(Vector2.zero);
+            _topRight = camera.ViewportToWorldPoint(Vector2.one);
 
             _maxOrthographicSize = max;
         }
@@ -134,10 +161,17 @@ namespace Assets.Scripts
             var vDeltaExtent = deltaOrthographicSize;
             var hDeltaExtent = camera.aspect * vDeltaExtent;
 
-            Left = _bottomLeft.x + _hExtent - hDeltaExtent;
-            Right = _topRight.x - _hExtent + hDeltaExtent;
-            Bottom = _bottomLeft.y + _vExtent - vDeltaExtent;
-            Top = _topRight.y - _vExtent + vDeltaExtent;
+            _left = _bottomLeft.x + _hExtent - hDeltaExtent;
+            _right = _topRight.x - _hExtent + hDeltaExtent;
+            _bottom = _bottomLeft.y + _vExtent - vDeltaExtent;
+            _top = _topRight.y - _vExtent + vDeltaExtent;
+        }
+
+        public Vector3 ClampCamera(Vector3 position)
+        {
+            position.x = Mathf.Clamp(position.x, _left, _right);
+            position.y = Mathf.Clamp(position.y, _bottom, _top);
+            return position;
         }
     }
 
